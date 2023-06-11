ee1a37298f78873dd176ad958cff9daa33e83036
JeozadaqueMacedo <jam6@discente.ifpe.edu.br>
2023-06-11 15:37:52 -0300

    New Branch added commit

--- GCMS Pratica Git/src/HelloWorld.java
+++ GCMS Pratica Git/src/HelloWorld.java
@@ -2,7 +2,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World! Teste");
+		System.out.println("Hello World!!");
+		System.out.println("New Branch Added!");
 	}
 
 }
